e46a9ae4f8c0488fe7012ea56f651170fee868c7
Gaëtan H <steellgold0@gmail.com>
2022-12-22 17:17:45 +0100

    Remove buttons and add a TODO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,16 +10,12 @@
 <main class="container mx-auto px-2 sm:px-4 py-4">
   <nav class="bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-    <a href="/" class="flex items-center">
+      <a href="/" class="flex items-center">
         <img src="https://cdn.discordapp.com/attachments/927843710669062204/1055460347282194492/Light_Logo_TaskBoard_128.png" class="h-10 mr-3 sm:h-9" alt="Flowbite Logo" />
-    </a>
-    <div class="flex md:order-2">
-      <button
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Nouvelle tâche
-      </button>
-    </div>
+      </a>
+      <div class="flex md:order-2">
+        <!-- TODO: Add button for connect (with GitHub? or Discord?) -->
+      </div>
     </div>
   </nav>
 
