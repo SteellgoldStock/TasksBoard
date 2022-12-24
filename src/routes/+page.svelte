60bd4c40bc3d1c8c33e8ab7340db24087360e991
Gaëtan H <steellgold0@gmail.com>
2022-12-24 17:05:39 +0100

    🏠 Home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,12 @@
-<script lang="ts">
-  import { Task } from "$lib/components/task";
-	
-  let tasks = [
-		{ title: 'Built for Error', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, eius.", id: "built-for-error", addedAt: 1671633179722 },
-		{ title: 'Secret of the Three-Inch Inspector', description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, modi! Quidem temporibus fugit repellat perspiciatis obcaecati non aliquid illum error. Quasi aperiam eveniet quod eligendi quidem doloremque iste cumque atque.", id: "secret-of-the-three-inch-inspector", addedAt: 1671891279722 },
-		{ title: 'Sugar and Spice', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero temporibus modi cum quo aliquid autem illum deserunt sit qui? Quaerat molestiae asperiores, deleniti omnis distinctio adipisci debitis nam maxime. Cumque sint nemo culpa voluptatum. Libero voluptatibus tenetur necessitatibus tempora quod, sit natus quisquam consectetur beatae, quos odit, qui maxime dolores!", id: "sugar-and-spice", addedAt: 1097789160000 },
-		{ title: 'Mister Nice', description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore maiores obcaecati deserunt, fugit debitis esse magnam nam alias eos cupiditate!", id: 'mister-nice', addedAt: 1578466980000 },
-		{ title: 'Micha Micha', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, assumenda.", id: 'micha-micha', addedAt: 1578466980000 },
-		{ title: 'Zoubiyoulou', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit error consequuntur id. Qui nobis, cumque dolore ipsam earum sapiente velit accusantium voluptatem molestiae molestias et ab nemo inventore. Possimus, accusamus.", id: 'zoubiyoulou', addedAt: 1578414778500 },
-		{ title: 'Pokémon', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, laborum veniam necessitatibus non quaerat et ab quod soluta reprehenderit qui facilis? Dolorum, harum voluptatum. Dicta dolorum quasi incidunt labore totam maiores sed magnam, voluptatum delectus architecto consequatur enim sunt?", id: 'pokemon', addedAt: 1578965888700 }
-	];
-</script>
+<section class="bg-white dark:bg-gray-900">
+  <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div class="mr-auto place-self-center lg:col-span-7">
+          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Your new task management tool</h1>
+          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Use it and make it easy for yourself to know what task you have to do that day?</p>
+      </div>
 
-<div class="grid">
-  {#each tasks as task}
-    <Task props={{ title: task.title, description: task.description, identifier: task.id, addedAt: task.addedAt }} />
-  {/each}
-</div>
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/BLANK_ICON.png" alt="mockup">
+      </div>
+  </div>
+</section>
