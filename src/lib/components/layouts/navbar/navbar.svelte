3cbc7e7cbadfeaba18ef3c707e58a3629976ee41
Gaëtan H <steellgold0@gmail.com>
2022-12-24 17:11:15 +0100

    🌙 Dark theme for all (no light theme)

--- src/lib/components/layouts/navbar/navbar.svelte
+++ src/lib/components/layouts/navbar/navbar.svelte
@@ -1,8 +1,8 @@
-<nav class="bg-white border-gray-200 pt-4 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+<nav class="bg-transparent pt-4 px-2 sm:px-4 py-2.5 rounded">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
     <a href="https://flowbite.com/" class="flex items-center">
-      <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <img src="/Light Logo TaskBoard 128.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">TasksBoard</span>
     </a>
     <div class="flex md:order-2">
       <div class="flex items-center justify-center space-x-4">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<section class="bg-white dark:bg-gray-900">
+<section>
   <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
       <div class="mr-auto place-self-center lg:col-span-7">
           <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Your new task management tool</h1>
