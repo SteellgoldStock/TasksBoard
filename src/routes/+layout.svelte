7dbdc87c9e6750b800d56cd6b34e68e7b833cdc6
Gaëtan H <steellgold0@gmail.com>
2022-12-24 15:30:34 +0100

    🗑️ Rework page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,7 @@
 <script lang="ts">
   import "../app.css";
-
-  let show = false;
-  function toggle() {
-    show = !show;
-  }
 </script>
 
 <main class="container mx-auto px-2 sm:px-4 py-4">
-  <nav class="bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-    <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="/" class="flex items-center">
-        <img src="https://cdn.discordapp.com/attachments/927843710669062204/1055460347282194492/Light_Logo_TaskBoard_128.png" class="h-10 mr-3 sm:h-9" alt="Flowbite Logo" />
-      </a>
-      <div class="flex md:order-2">
-        <!-- TODO: Add button for connect (with GitHub? or Discord?) -->
-        <button on:click={toggle} type="button" class="text-white border-2 border-gray-500 hover:border-gray-300 bg-gray-700 dark:border-gray-700 dark:bg-gray-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 hover:transition-all duration-500">Nouvelle tâche</button>
-      </div>
-    </div>
-  </nav>
-
-  {#if show}
-    <div class="grid grid-cols-2 md:grid-cols-6">
-      <div class="col-span-3 sm:col-span-2 lg:col-span-1 xl:col-span-1">
-        <p class="text-2xl font-bold text-white">Nouvelle tâche</p>
-      </div>
-      <div class="col-span-3 sm:col-span-4 lg:col-span-5 xl-col-span-5">
-        <slot />
-      </div>
-    </div>
-  {:else}
-    <div class="container mx-auto px-2 sm:px-4 py-4">
-      <slot />
-    </div>
-  {/if}
-  
+  <slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 	];
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3">
+<div class="grid">
   {#each tasks as task}
     <Task props={{ title: task.title, description: task.description, identifier: task.id, addedAt: task.addedAt }} />
   {/each}
