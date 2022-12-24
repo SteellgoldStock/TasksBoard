a6546eb8ace43a97a7b2bda51658314d95690b4c
Gaëtan H <steellgold0@gmail.com>
2022-12-24 17:04:56 +0100

    💻 Naviguation bar

---
+++ src/lib/components/layouts/navbar/navbar.svelte
@@ -0,0 +1,16 @@
+<nav class="bg-white border-gray-200 pt-4 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <div class="container flex flex-wrap items-center justify-between mx-auto">
+    <a href="https://flowbite.com/" class="flex items-center">
+      <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+    </a>
+    <div class="flex md:order-2">
+      <div class="flex items-center justify-center space-x-4">
+        <a href="/" class="flex items-center space-x-1 font-semibold text-white hover:text-gray-50"> 
+          <span>Connexion</span>
+        </a>
+        <a href="/" class="inline-block px-5 py-2 font-semibold text-white rounded-lg bg-gradient-to-br from-indigo-600 to-indigo-700 hover:from-indigo-500 hover:to-indigo-700 ">Inscription</a>
+      </div>
+    </div>
+  </div>
+</nav>
