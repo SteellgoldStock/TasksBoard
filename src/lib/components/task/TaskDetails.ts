bca57e45b02e06aef247f22e6cf134572e3a28b5
Gaëtan H <steellgold0@gmail.com>
2022-12-25 17:06:55 +0100

    🗜️ master -> preview -> master

--- src/lib/components/task/TaskDetails.ts
+++
@@ -1,6 +0,0 @@
-export interface TaskDetailsProps {
-  title: string;
-  description: string;
-  addedAt: number;
-  identifier: string;
-}
--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import "/src/app.css";
-  import { Navbar } from "$lib/components/layouts/navbar";
-</script>
-
-<main class="bg-gradient-to-b from-[#111827] to-black">
-  <Navbar />
-  <slot></slot>
-</main>
