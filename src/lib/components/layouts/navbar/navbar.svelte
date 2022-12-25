fa7149f5bff2d2de6006cce1702afb950cde60d3
Gaëtan H <steellgold0@gmail.com>
2022-12-25 13:01:31 +0100

    🌙 Only dark theme

--- src/lib/components/layouts/navbar/navbar.svelte
+++ src/lib/components/layouts/navbar/navbar.svelte
@@ -1,15 +1,13 @@
-<nav class="bg-transparent pt-4 px-2 sm:px-4 py-2.5 rounded">
+<nav class="bg-transparent pt-4 px-2 sm:px-4 py-2.5">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
-    <a href="https://flowbite.com/" class="flex items-center">
-      <img src="/Light Logo TaskBoard 128.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">TasksBoard</span>
+    <a href="/" class="flex items-center">
+      <img src="/Light Logo TaskBoard 128.png" class="h-7 mr-3 sm:h-9" alt="Flowbite Logo" />
     </a>
     <div class="flex md:order-2">
       <div class="flex items-center justify-center space-x-4">
-        <a href="/" class="flex items-center space-x-1 font-semibold text-white hover:text-gray-50"> 
-          <span>Connexion</span>
+        <a href="/" class="inline-block px-5 py-2 font-semibold text-white  rounded-lg bg-gradient-to-br from-blue-600 to-blue-700 hover:from-blue-500 hover:to-blue-700">
+          Inscription
         </a>
-        <a href="/" class="inline-block px-5 py-2 font-semibold text-white rounded-lg bg-gradient-to-br from-indigo-600 to-indigo-700 hover:from-indigo-500 hover:to-indigo-700 ">Inscription</a>
       </div>
     </div>
   </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <section>
   <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
       <div class="mr-auto place-self-center lg:col-span-7">
-          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Your new task management tool</h1>
+          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold text-white tracking-tight leading-none md:text-5xl xl:text-6xl">Your new task management tool</h1>
           <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Use it and make it easy for yourself to know what task you have to do that day?</p>
       </div>
 
