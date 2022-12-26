c81a9e24a00cd6f8153f2721e4d8f8d8d33faf9f
Gaëtan H <steellgold0@gmail.com>
2022-12-26 17:52:31 +0100

    🛰️ POST (Create Task) API function

--- src/routes/api/tasks/+server.ts
+++ src/routes/api/tasks/+server.ts
@@ -11,6 +11,37 @@ export async function GET({ url } : RequestEvent) : Promise<Response> {
     if (Object.keys(task).length === 0) return new Response('Not found', { status: 404 });
     else return new Response(JSON.stringify(task), { status: 200 });
   } else return new Response("Parameter ID missing", { status: 400 });
+}
+
+export async function POST({ url, request } : RequestEvent) : Promise<Response> {
+  if (url.searchParams.get('authToken') !== TEMP_AUTHENTIFICATION_TOKEN) return new Response('Unauthorized', { status: 401 });
+  const values = await request.json();
+
+  // Comments: Ideas for future improvements
+  const bodySchema = z.object({
+    id: z.string(),
+    title: z.string().min(3).max(25),
+    description: z.string(),
+    // status: z.string(),
+    // priority: z.string(),
+    // dueDate: z.string(),
+    addedAt: z.string(),
+    // completedAt: z.string(),
+  });
+  
+  const bodyParsed = bodySchema.safeParse(values);
+  if (!bodyParsed.success) return new Response('Bad request: \n' + bodyParsed.error.message, { status: 400 });
+
+  const task = Tasks.find(task => task.id === values.id);
+  if (task) {
+    return new Response(`Task ID ${bodyParsed.data.id} already exists`, { status: 409 });
+  } else {
+    values.addedAt = Number.parseInt(values.addedAt);
+    Tasks.push(values);
+    return new Response(`Task ID ${bodyParsed.data.id} successfuly added`, { status: 201 });
+  }
+}
+
 export async function PUT({ url, request } : RequestEvent) : Promise<Response> {
   if (url.searchParams.get('authToken') !== TEMP_AUTHENTIFICATION_TOKEN) return new Response('Unauthorized', { status: 401 });
   const values = await request.json();
