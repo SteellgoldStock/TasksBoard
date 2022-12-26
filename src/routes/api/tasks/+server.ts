fb08bdc7ea04d26c4576397d95632524853a3714
Gaëtan H <steellgold0@gmail.com>
2022-12-26 17:50:22 +0100

    🩹 Simplify code

--- src/routes/api/tasks/+server.ts
+++ src/routes/api/tasks/+server.ts
@@ -3,19 +3,13 @@ import { Tasks } from './tasks';
 import { TEMP_AUTHENTIFICATION_TOKEN } from '$env/static/private';
 
 export async function GET({ url } : RequestEvent) : Promise<Response> {
-  if (url.searchParams.get('authToken') !== TEMP_AUTHENTIFICATION_TOKEN) {
-    return new Response('Unauthorized', { status: 401 });
-  }
+  if (url.searchParams.get('authToken') !== TEMP_AUTHENTIFICATION_TOKEN) return new Response('Unauthorized', { status: 401 });
 
   if (url.searchParams.get('id')) {
     const task = Tasks.find(task => task.id === url.searchParams.get('id')) || {};
-    
-    if (Object.keys(task).length === 0) {
-      return new Response('Not found', { status: 404 });
-    }
-    
-    return new Response(JSON.stringify(task), { status: 200 });
+    if (Object.keys(task).length === 0) return new Response('Not found', { status: 404 });
+    else return new Response(JSON.stringify(task), { status: 200 });
+  } else return new Response("Parameter ID missing", { status: 400 });
   }
 
-  return new Response("Missing parameter 'id'", { status: 400 });
 }
