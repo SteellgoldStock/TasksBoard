0e3ad4f2b0851799fe34c301f657ae54f6147e70
Gaëtan H <steellgold0@gmail.com>
2022-12-28 17:19:20 +0100

    📑 Note for later (Auth System)

--- src/lib/utils/Env.ts
+++ src/lib/utils/Env.ts
@@ -1,9 +1,8 @@
 import { URL, TEMP_AUTHENTIFICATION_TOKEN } from "$env/static/private";
 
 export function parseURL(end_url: string, https: boolean = false) {
-  // AUTH_TOKEN is temporary, will be replaced by a token given by the user
-  // if token is not valid, return 401
-  // if token is valid, return the data with 200
   let authToken = (end_url.includes("?") ? "&" : "?") + "authToken=" + TEMP_AUTHENTIFICATION_TOKEN;
   return (https ? "https://" : "http://") + URL + "/" + end_url + authToken;
+
+  // TODO: Make an authentication system to have real data, not via environment variables
 }
