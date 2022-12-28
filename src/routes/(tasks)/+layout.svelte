a7eff7d4c0fdabc8865d004d0289bf2e5116a569
Gaëtan H <steellgold0@gmail.com>
2022-12-28 17:20:00 +0100

    📑 Comment a old code, to need a update

--- src/routes/(tasks)/+layout.svelte
+++ src/routes/(tasks)/+layout.svelte
@@ -15,10 +15,10 @@
   </nav>
 
   <div class="container mx-auto px-2 sm:px-4 py-4">
-    <div class="columns-2">
+    <!-- <div class="columns-2">
       <div>1</div>
       <div>2</div>
-    </div>
+    </div> -->
     <slot />
   </div>  
 </main>
