a718619c338dc721c8c4543274f076b10793ab6b
Gaëtan H <steellgold0@gmail.com>
2023-01-06 17:56:06 +0100

    🖌️ Colors configuration

--- src/lib/components/task/task.svelte
+++ src/lib/components/task/task.svelte
@@ -2,14 +2,23 @@
   import dayjs from 'dayjs';
   import type { TaskDetailsProps } from './TaskDetails';
   import { removeClasses, addClasses } from '$lib/utils/Classes';
+    import clsx from 'clsx';
   
   export let props : TaskDetailsProps;
+
   let date = dayjs(props.createdAt).format('DD/MM/YYYY');
   let hour = dayjs(props.createdAt).format('HH');
   let minute = dayjs(props.createdAt).format('mm');
   
   export let completed: boolean = false;
-  function changeColor() {
+  let isCompletedColor: string = clsx({
+    "bg-green-500 hover:bg-green-400": !completed,
+    "bg-rose-500 hover:bg-rose-400": completed,
+  });
+
+  function completeTask() {
+    // TODO: Update task status with API
+
     // @ts-ignore
     let element: HTMLElement = document.getElementsByClassName(props.taskIdentifier)[0];
 
@@ -25,16 +34,16 @@
   }
 </script>
 
-<div class="mb-3 border-2 {completed ? "border-dashed" : ""} border-gray-500 bg-gray-700 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:border-gray-300 hover:transition-all duration-500 {completed ? "order-last opacity-30 hover:opacity-60" : ""}">
+<div class="mb-3 border-2 {completed ? "border-dashed" : ""} border-default-light-border bg-default-secondary rounded-lg shadow-lg dark:bg-dark-secondary dark:border-gray-700 hover:border-default-light-border-hover hover:transition-all duration-500 {completed ? "order-last opacity-30 hover:opacity-60" : ""}">
   <div class="px-4 py-5 sm:p-6 md:p-4">
     <div class="flex items-center gap-3">
-      <div class="{props.taskIdentifier} flex-shrink-0 bg-green-500 rounded-md p-3 hover:bg-green-400 hover:transition-all duration-500" on:click={changeColor} on:keypress={changeColor}>
+      <div class="{props.taskIdentifier} {isCompletedColor} flex-shrink-0 rounded-md p-3 hover:transition-all duration-500" on:click={completeTask} on:keypress={completeTask}>
         <svg class="h-3 w-3 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M5 15l4 4L19 7" />
         </svg>
       </div>
 
-      <p class="text-sm leading-5 font-medium text-gray-200 dark:text-gray-50 truncate">{ props.taskTitle }</p>
+      <p class="text-sm leading-5 font-medium text-light dark:text-dark-light truncate">{ props.taskTitle }</p>
     </div>
     <p class="text-gray-300 mt-3">{ props.taskContent }</p>
   </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,50 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  darkMode: 'media',
+  darkMode: "media",
 
   content: [
-    './src/**/*.{html,js,svelte,ts}'
+    "./src/**/*.{html,js,svelte,ts}"
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#111827',
-        'secondary': '#1F2937',
-        'danger': '#b91c1c',
-        'success': '#15803d',
-        'warning': '#fbbf24',
-        'info': '#0369a1',
+        "default": {
+          "primary": "#1f2937",
+          "secondary": "#374151",
+          "danger": "#dc2626",
+          "success": "#047857",
+          "warning": "#f59e0b",
+          "info": "#075985",
+          "light": {
+            DEFAULT: "#e2e8f0",
+            "border": {
+              DEFAULT: "#64748b",
+              "hover": "#cbd5e1"
+            }
+          }
+        },
+
+        "dark": {
+          "primary": "#111827",
+          "secondary": "#1f2937",
+          "danger": "#ef4444",
+          "success": "#10b981",
+          "warning": "#fbbf24",
+          "info": "#0891b2",
+          "light": {
+            DEFAULT: "#f8fafc",
+            "border": {
+              DEFAULT: "#374151",
+              "hover": "#cbd5e1"
+            }
+          }
+        }
       }
     }
   },
   plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+    // eslint-disable-next-line no-undef
+    require("@tailwindcss/forms")
+  ]
+};
