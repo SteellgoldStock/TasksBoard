0a3ea8dd068e67460a4e28f9bf2293c59f442275
Gaëtan H <steellgold0@gmail.com>
2023-01-13 15:17:46 +0100

    🛞 Add Layout for /(user)/

---
+++ src/routes/(user)/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Navbar } from "$lib/components/layout/navbar";
+  import "/src/app.css";
+</script>
+
+<main class="container mx-auto px-2 sm:px-4 py-4">
+  <Navbar />
+  <div class="container mx-auto sm:px-4 py-4">
+    <slot />
+  </div>  
+</main>
