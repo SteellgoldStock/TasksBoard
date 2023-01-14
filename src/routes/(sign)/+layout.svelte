822d77d215b94ef0ad3d267e05cb2503b9a8268b
Gaëtan H <steellgold0@gmail.com>
2023-01-14 12:39:19 +0100

    🧬 Layout for Sign(in-up) and some things

---
+++ src/routes/(sign)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Navbar } from "$lib/components/layout/navbar";
+  import "/src/app.css";
+</script>
+
+<main class="container mx-auto px-2 sm:px-4 py-4">
+  <Navbar />
+  <div class="container mx-auto sm:px-4 py-4">
+    <div class="flex flex-col items-center md:h-screen lg:py-0">
+      <div class="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+        <slot />
+      </div>
+    </div>
+  </div>  
+</main>
