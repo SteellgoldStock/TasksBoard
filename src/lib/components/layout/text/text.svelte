d4456b258eaa0266645a7375c81c99dfa216bd7b
Gaëtan H <steellgold0@gmail.com>
2023-01-14 12:39:48 +0100

    🗑️ Remove dark/light theme

--- src/lib/components/layout/text/text.svelte
+++ src/lib/components/layout/text/text.svelte
@@ -2,7 +2,7 @@
   export let size: "xs" | "sm" | "base" | "lg" | "xl" | "2xl"  | "2xl" | "4xl" | "5xl" | "6xl" | "7xl" | "8xl" | "9xl" = "base";
   export let font: "thin" | "extralight" | "light" | "normal" | "medium" | "semibold" | "bold" | "extrabold" | "black" = "normal";
   export let align: "left" | "center" | "right" | "justify" | "start" | "end" = "center";
-  export let color: string = "text-gray-900 dark:text-gray-400";
+  export let color: string = "text-gray-400";
 </script>
 
 <p class="text-{size} font-{font} text-{align} {color}">
